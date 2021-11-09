ad0388b4899b5f946f57c2b01d28a71ff74e6555
Christopher <chris@bagge.com>
2021-11-09 09:20:13 -0500

    object key

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import pokemon from "./pokemon.json";
+import { Name, Base, IPokemon } from "./interfaces/pokemon";
+
+const PokemonRow = ({ pokemon }: { pokemon: IPokemon }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+  </tr>
+);
+
+function PokemonRow2({
+  pokemon,
+  onSelect,
+}: {
+  pokemon: IPokemon;
+  onSelect: (pokemon: IPokemon) => void;
+}) {
+  return (
+    <tr>
+      <td>{pokemon.name.english}</td>
+      <td>{pokemon.type.join(", ")}</td>
+      <td>
+        <button onClick={() => onSelect(pokemon)}>Select!</button>
+      </td>
+    </tr>
+  );
+}
+
+function PokemonInfo({ name, base }: { name: Name; base: Base }) {
+  type TypeKey = "HP" | "Attack";
+  let kindX: keyof Base;
+
+  let k: keyof Base;
+
+  for (k in base) {
+    console.log(k, base[k]);
+  }
+
+  return (
+    <div>
+      <h1>{name.english}</h1>
+      <table>
+        {Object.keys(base).map((key) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{base[key]}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
 
 function App() {
+  const [filter, filterSet] = useState("");
+  const [selectedItem, selectedItemSet] = useState<IPokemon | null>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem",
+      }}
+    >
+      <h1 className="title">Pokemon search</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "70% 30%",
+          columnGap: "1rem",
+        }}
+      >
+        <div>
+          <input value={filter} onChange={(evt) => filterSet(evt.target.value)} />
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* {pokemon.map((pokemon) => ( */}
+              {pokemon
+                .filter((pokemon) =>
+                  pokemon.name.english.toLowerCase().includes(filter.toLowerCase())
+                )
+                .slice(0, 20)
+                .map((pokemon) => (
+                  <PokemonRow2
+                    pokemon={pokemon}
+                    key={pokemon.id}
+                    onSelect={(pokemon) => selectedItemSet(pokemon)}
+                  />
+                ))}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && <PokemonInfo {...selectedItem} />}
+      </div>
     </div>
   );
 }
---
+++ src/interfaces/pokemon.ts
@@ -0,0 +1,23 @@
+export interface Name {
+  english: string;
+  japanese: string;
+  chinese: string;
+  french: string;
+}
+
+export interface Base {
+  HP: number;
+  Attack: number;
+  Defense: number;
+  "Sp. Attack": number;
+  "Sp. Defense": number;
+  Speed: number;
+  [key: string]: number;
+}
+
+export interface IPokemon {
+  id: number;
+  name: Name;
+  type: string[];
+  base: Base;
+}
